cc07357d95822fb8d3a87b30cfc06710823bb87c
Ahmad <asusahmad26@gmail.com>
2025-11-30 16:54:03 +0000

    update

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -3,9 +3,4 @@ def islower(c):
     if c >= 'a' and c <= 'z':
         return True
     else:
-        return False
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
+      return False
