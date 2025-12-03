66765eec70002cf1acbf07d62164cb8373a8b38b
Ahmad <asusahmad26@gmail.com>
2025-12-03 14:08:39 +0000

    update

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    argv = sys.argv
+    argv = sys.argv[1:]
     result = 0
-    for i in argv:
-        result += int(i)
-    print(result)
+    for i in range(0, len(argv)):
+        result += int(argv[i])
+    print(result, end='\n')
 
