e5861785eb45ed7000810f0f02541ed8af7aa20c
Ahmad <asusahmad26@gmail.com>
2025-12-03 13:40:35 +0000

    update

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+if __name__ = "__main__":
+    argv = sys.argv[1:]
+    result = 0
+    for i in range(0, len(argv)+1):
+        result += int(i)
+    print(result)
+
