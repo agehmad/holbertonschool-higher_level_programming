d81e21000b0bba881ec97fd2ddb8062b6cafa8f0
Ahmad <asusahmad26@gmail.com>
2025-12-07 13:28:48 +0400

    1

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     row = len(matrix)
     col = len(matrix[0])
     for i in range(0, row):
-        for j in range(0,col):
-            print("{:d}".format(matrix[i][j]), end=" ")
+        for j in range(0, col):
+            endd = "" if j == col -1 else " "
+            print("{:d}".format(matrix[i][j]), end=endd)
         print()
