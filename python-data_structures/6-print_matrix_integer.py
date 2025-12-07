a651e473e8f8e9a924b80c00702b007eff36f292
Ahmad <asusahmad26@gmail.com>
2025-12-07 13:09:13 +0400

    1

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    row = len(matrix)
+    col = len(matrix[0])
+    for i in range(0, row):
+        for j in range(0,col):
+            print("{}".format(matrix[i][j]), end=" ")
+        print("\n")
