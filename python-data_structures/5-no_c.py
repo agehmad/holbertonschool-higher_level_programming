6b9af046ce3c14ec7fe4573afc168822e8ed8562
Ahmad <asusahmad26@gmail.com>
2025-12-07 11:46:51 +0400

    1

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,11 @@
-#!/usr/bin/python3
+# !/usr/bin/python3
 def no_c(my_string):
     new_string = ""
     for i in range(0,len(my_string)):
-        if my_string[i] == 'c' or 'C':
-            new_string[i] = ''
+        if my_string[i] == 'c' or my_string[i] == 'C':
+            new_string += ''
         else:
-            new_string[i] = my_string[i]
+            new_string += my_string[i]
     return new_string
+word = "School"
+print(no_c(word))
