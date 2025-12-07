0b619c271860ffc373deec982f43e2fdb17f67d0
Ahmad <asusahmad26@gmail.com>
2025-12-07 23:14:21 +0400

    1

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    alphabet = {'M': 1000,'D': 500,'C': 100,'L': 50,'X': 10,'V': 5,'I': 1}
-    result = 0
-    if roman_string.isalpha() or roman_string == None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    else:
-        for i in roman_string:
-            result += alphabet[i]
+    alphabet = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    result = 0
+    n = len(roman_string)
+    for i in range(n):
+        current_value = alphabet[roman_string[i]]
+        if i < n - 1:
+            next_value = alphabet[roman_string[i + 1]]
+        else:
+            next_value = 0  
+        if current_value < next_value:
+            result -= current_value
+        else:
+            result += current_value
+    return result
