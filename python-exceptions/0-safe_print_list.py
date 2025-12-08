9f36a830218b043c2d87440842fdcd9c13549673
Ahmad <asusahmad26@gmail.com>
2025-12-08 17:40:37 +0400

    1

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    n = 0 #number of real elements
+    n = 0
     for i in range(0, x):
         try:
             print(my_list[i], end='')
             n += 1
-            continue
         except IndexError:
             break
-    print('\n', n)
+    print()
+    print(n)
