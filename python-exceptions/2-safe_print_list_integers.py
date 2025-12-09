2b4877ec28770a6b6dca1ee9c76479470986e35a
Ahmad <asusahmad26@gmail.com>
2025-12-09 14:50:21 +0400

    1

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,4 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
                 n += 1
             except (IndexError, TypeError, ValueError):
                 continue
+        print()
         print(n)
