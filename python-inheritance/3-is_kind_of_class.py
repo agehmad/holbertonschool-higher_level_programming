e331cbea93aa7d4ad0a182afa402590d45b2b90b
Ahmad <asusahmad26@gmail.com>
2025-12-14 20:49:45 +0400

    1

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -7,7 +7,7 @@ Docstring for python-inheritance.3-is_kind_of_class
 def is_kind_of_class(obj, a_class):
     """
     Docstring for is_kind_of_class
-    
+
     :param obj: Description
     :param a_class: Description
     """
