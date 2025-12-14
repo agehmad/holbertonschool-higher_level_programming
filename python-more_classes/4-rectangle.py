e32928d354641bdabcd92dbd58df01427564100f
Ahmad <asusahmad26@gmail.com>
2025-12-14 16:17:23 +0400

    1

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+Docstring for python-more_classes.4-rectangle
+"""
+
+
+class Rectangle:
+    """
+    Docstring for Rectangle
+    """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Getter for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Getter for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.__height*self.__width
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return (2*(self.__height + self.__width))
+
+    def __str__(self):
+        if self.__height == 0 or self.__width == 0:
+            print()
+        else:
+            for i in range(self.__height):
+                print('#' * self.__width)
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height) 
