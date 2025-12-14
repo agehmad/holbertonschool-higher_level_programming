7bbc8a8ba1f84f2621a0b2a46481d67a042c70e8
Ahmad <asusahmad26@gmail.com>
2025-12-14 21:01:02 +0400

    1

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Docstring for python-inheritance.6-base_geometry
+"""
+
+
+class BaseGeometry:
+    """
+    Docstring for BaseGeometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
