d1992b10bac4a00e5115d71de7dd594625bdd7b1
Ahmad <asusahmad26@gmail.com>
2025-12-14 20:57:33 +0400

    1

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,7 +7,7 @@ Docstring for python-inheritance.4-inherits_from
 def inherits_from(obj, a_class):
     """
     Docstring for inherits_from
-    
+
     :param obj: Description
     :param a_class: Description
     """
