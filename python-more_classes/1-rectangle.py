25ad964f3a770088360250fb7953212196a25552
Ahmad <asusahmad26@gmail.com>
2025-12-14 15:32:34 +0400

    1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -9,23 +9,33 @@ class Rectangle:
     Docstring for Rectangle
     """
     def __init__(self, width=0, height=0):
-        self.set_width(width)
-        self.set_height(height)
-    def set_width(self, value):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        elif not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        else:
-            self.__width = value
-    def set_height(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        elif not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        else:
-            self.__height = value
-    def get_width(self):
-        return self.__width
-    def get_height(self):
-        return self.__height
+        self.__height = value
