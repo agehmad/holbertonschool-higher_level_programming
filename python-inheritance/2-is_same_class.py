7cee3aa5479fd002cc13bcb94d0ac474d67eee2f
Ahmad <asusahmad26@gmail.com>
2025-12-14 20:11:47 +0400

    1

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Docstring for python-inheritance.2-is_same_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Docstring for is_same_class
+    
+    :param obj: Description
+    :param a_class: Description
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
