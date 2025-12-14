a2183a8b3e3c13eacff14dc22a4fbd7e12269f70
Ahmad <asusahmad26@gmail.com>
2025-12-14 15:13:44 +0400

    1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Docstring for python-more_classes.0-rectangle
+"""
+
+
+class Rectangle:
+    """
+    Docstring for Rectangle
+    """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+    def set_width(self, value):
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        elif not isinstance(value, int):
+            raise TypeError("widt must be an integer")
+        else:
+            self.__width = value
+    def set_height(self, value):
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        elif not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        else:
+            self.__height = value
+    def get_width(self):
+        return self.__width
+    def get_height(self):
+        return self.__height
