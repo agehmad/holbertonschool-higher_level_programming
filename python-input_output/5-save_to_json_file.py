dd5fcfcf04bcbce55a09aa48cd47b1b74a0ed80d
Ahmad <asusahmad26@gmail.com>
2025-12-16 20:07:26 +0400

    1

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Docstring for python-input_output.5-save_to_json_file
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Docstring for save_to_json_file
+
+    :param my_obj: Description
+    :param filename: Description
+    """
+    
+    with open(filename, mode='w', encoding='utf-8') as a_file:
+        a_file.write(json.loads(my_obj))
