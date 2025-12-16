8b6e317b7565cfe074c151a33a04cb9e79523fde
Ahmad <asusahmad26@gmail.com>
2025-12-16 20:03:36 +0400

    1

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Docstring for python-input_output.4-from_json_string
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Docstring for from_json_string
+
+    :param my_str: Description
+    """
+    return json.loads(my_str)
