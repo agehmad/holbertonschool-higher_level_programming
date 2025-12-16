4999f46674b360fe191e860cb86b488bff30136d
Ahmad <asusahmad26@gmail.com>
2025-12-16 18:17:54 +0400

    1

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def read_file(filename=""):
+    """
+    Docstring for read_file
+    
+    :param filename: Description
+    """
     with open(filename, encoding='utf-8') as a_file:
         print(a_file.read())
