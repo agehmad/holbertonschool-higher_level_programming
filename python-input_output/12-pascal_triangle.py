c7eb5a1e9129062a59b09cad0b57bca47411024f
Ahmad <asusahmad26@gmail.com>
2025-12-20 19:29:13 +0400

    1

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,25 @@
+# !/usr/bin/python3
+"""
+Docstring for python-input_output.12-pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Docstring for pascal_triangle
+
+    :param n: Description
+    """
+    if n <= 0:
+        return []
+    else:
+        triangle = [[1], [1,1]]
+        for i in range(2, n):
+            newlist = []
+            newlist.append(1)
+            for j in range(0, i-1):
+                newlist.append(triangle[i-1][j] + triangle[i-1][j+1])
+            newlist.append(1)
+            triangle.append(newlist)
+        for a in triangle:
+            print(a)
