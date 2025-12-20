027c06eb379083285c5b13b42f13bbaf176d96dd
Ahmad <asusahmad26@gmail.com>
2025-12-20 14:14:56 +0400

    1

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -20,5 +20,5 @@ class Student:
         return self.__dict__
 
     def reload_from_json(self, json):
-        self.__dir__ = json
-        return self.__dir__
+        for key, value in json.items():
+            setattr(self, key, value)
