51f3d7ed05bfaefd99d1e95216e71ca2aad38f39
Ahmad <asusahmad26@gmail.com>
2025-12-20 13:33:08 +0400

    1

---
+++ python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Docstring for python-input_output.10-student
+"""
+
+
+class Student:
+    """
+    Docstring for Student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(item, str) for item in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
