f5b3fb8d69628463bfa77bcde3af73f7597ef413
Ahmad <asusahmad26@gmail.com>
2025-12-21 15:06:24 +0400

    1

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -6,9 +6,9 @@ def convert_csv_to_json(cfilename):
         with open(cfilename, 'r') as cfile:
             reader = csv.DictReader(cfile)
             data = list(reader)
-        with open('data.json', 'w') as jfile:
-            json.dump(line, jfile)
-            return True
+            with open('data.json', 'w') as jfile:
+                json.dump(line, jfile)
+                return True
     except FileNotFoundError:
         return False
   
